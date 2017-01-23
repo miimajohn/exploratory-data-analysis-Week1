b5d72f55e2d17f1908c983ae367572ec7dfcede5
John Miima <miimajohn@gmail.com>
2017-01-22 23:34:38 -0500

    Update Plot_1.R

--- Plot_1.R
+++ Plot_1.R
@@ -1,1 +1,9 @@
-## Plot1
+dataFile <- "/Users/mjb/Documents/DocuR/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalPower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalPower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
